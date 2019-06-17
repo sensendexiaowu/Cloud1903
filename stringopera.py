9116803cd8be99fee490018628141c57b0885f64
sensendexiaowu <hansencheng@tom.com>
2019-06-17 14:00:42 +0800

    add stringopera.py

---
+++ stringopera.py
@@ -0,0 +1,12 @@
+str01 = "python is good language"
+
+str01_split = str01.split()
+print(str01_split, type(str01_split))
+
+str01_replace = str01.replace('o', '0')
+print(str01_replace)
+
+if "o" in str01:
+    print("o exist in str01")
+else:
+    print("o not exist in str01")
